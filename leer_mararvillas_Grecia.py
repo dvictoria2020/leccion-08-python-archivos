22b92a3273849c7580f0ffc1e69e7d3391e75108
dvictoria2020 <vicdelfer@gmail.com>
2020-09-17 17:57:06 -0600

    Agregar programa Grecia

--- leer_mararvillas_Grecia.py
+++ leer_mararvillas_Grecia.py
@@ -1,5 +1,5 @@
 # Recorrido e impresión de las líneas de un archivo de texto
 with open("maravillas_antiguas.csv", "r") as archivo:
     for linea in archivo:
-        print(linea, end='')
->>> print .find("Grecia ") 
+        if (linea.find("Grecia") != - 1):
+            print(linea, end='')
